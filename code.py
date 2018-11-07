545502e86c08c559ed3b8e4af955216aaf920fda
garg99 <35557113+gargnishith@users.noreply.github.com>
2018-11-08 03:49:59 +0530

    Update code.py

--- code.py
+++ code.py
@@ -4,6 +4,6 @@ from facebook import GraphAPI
 graph = GraphAPI("EAAacYxZBsn4sBAKlkiL421S4mRYwa8qgpAiXWgzVHg3K5B11KEbmJK15tb2MMjnrxTE3FA0iLXraIs0l6ZA35HdqwwQIBHAepn6QZCjB0nbhnZAZBcCvTjXVVrx3uugzZBfOgKJHAN0FHnUVu5LK2zGuvIxIZAon54YPFmcW9Tp5JI4ndiAY8tDv01ePoQVeRMZD")
 # graph = GraphAPI("access_token")
 try:
-print graph.get('me')
+  print graph.get('me')
 except FacebookClientError:
-print "failed!! :("
+  print "failed!! :("
